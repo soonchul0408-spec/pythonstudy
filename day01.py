68809962bd03f20c17b4cc354602e23477237d9f
백순철 <soonchul0408@gmail.com>
2026-07-17 18:03:04 +0900

    int사용->문자를 숫자로 인식하도록

--- day01.py
+++ day01.py
@@ -1,8 +1,2 @@
-month = int(input("월을 입력하세요 : "))
-if(month == 1 or month ==3 or month ==5 or month ==7 or month == 8 or month ==10 or month ==12):
-    print("31")
-elif (month == 2):
-    print("28")
-else:
-    print("30")
-
+a = int(input("숫자 입력 : "))
+print(a + a)
