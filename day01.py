72e4fd3e5df934f20012e85fc6ade8fa6553215b
백순철 <soonchul0408@gmail.com>
2026-07-17 17:50:45 +0900

    입력받은 정수를 차례로 출력

--- day01.py
+++ day01.py
@@ -1,7 +1,3 @@
-a = int(input("첫 번째 숫자 : "))
-b = int(input("두 번째 숫자 : "))
+numbers = input("숫자를 입력하세요 : ").split()
+print(numbers)
 
-# ??? 여기에 나머지를 구하는 코드를 작성
-result = a % b
-
-print("나머지 =", result)
