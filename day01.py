90783c953bfb74d772e022086cfc15dbed6179c0
백순철 <soonchul0408@gmail.com>
2026-07-17 17:58:06 +0900

    if-elif-else 와 print

--- day01.py
+++ day01.py
@@ -1,3 +1,8 @@
-numbers = input("숫자를 입력하세요 : ").split()
-print(numbers)
+month = int(input("월을 입력하세요 : "))
+if(month == 1 or month ==3 or month ==5 or month ==7 or month == 8 or month ==10 or month ==12):
+    print("31")
+elif (month == 2):
+    print("28")
+else:
+    print("30")
 
