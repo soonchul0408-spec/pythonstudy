60ac53d22dcac052531e6386a8ba7988cff0ab4b
백순철 <soonchul0408@gmail.com>
2026-07-17 20:13:03 +0900

    01-02 그냥해봅시다 ppt 끝

--- day01.py
+++ day01.py
@@ -1,15 +1,3 @@
-if height >= 140:
-    print("A")
-
-elif height >= 120:
-    print("B")
-
-else:
-    print("어린이")
-
-    # 컴퓨터는 조건이 만족하면 그 아래 함수는 더 이상 보지 않음 
-    # if 조건 만족하면 끝 
-    # if if if 쓰면 또 그 아래 조건 확인해서 이런 식으로 사용 x
-
-    # 조건을 각각 독립적으로 검사 -> if를 여러개 
-    # 여러 조건 중 하나만 선택 -> if - elif - else
+for x in range(30):
+    print("공부")
+    
