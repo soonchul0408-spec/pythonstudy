8b51199be022e9404a5af05ee87063e648b494fc
백순철 <soonchul0408@gmail.com>
2026-07-17 17:23:40 +0900

    나머지 연산자

--- day01.py
+++ day01.py
@@ -1,20 +1,7 @@
-print ("안녕하세요")
-
-print ("이제 잔디를 심어봅시다")
-
-print ("안녕하세요. 2015150265 백순철입니다")
-
-a = int(input("첫번쨰 숫자 : "))
-b = int(input("두번째 숫자 : "))
-
-result = a + b
-
-print(" 합계 =", result)
 a = int(input("첫 번째 숫자 : "))
 b = int(input("두 번째 숫자 : "))
 
-print("차 =", a - b)
-name = input("이름 : ")
-major = input("학과 : ")
+# ??? 여기에 나머지를 구하는 코드를 작성
+result = a % b
 
-print(name + "님의 학과는 " + major + "입니다.")
+print("나머지 =", result)
