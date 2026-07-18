f8c2f47050dc912b3e7feecb9f5fe52e605e049c
백순철 <soonchul0408@gmail.com>
2026-07-18 14:40:24 +0900

    스마일 그리기 연습

--- day0718.py
+++ day0718.py
@@ -4,10 +4,17 @@ s = turtle.Screen()
 s.setup(width=800, height=600)
 
 t = turtle.Turtle()
+t.circle(100)
+t.penup()
+t.goto(-40, 130)
+t.dot(15)
+t.goto(40, 130)
+t.dot(15)
+t.goto(-40, 60)
+t.pendown()
+t.right(90)
+t.circle(40, 180)
 
-t.forward(100)
+
 
 turtle.done()
-t.forward(100)
-t.left(90)
-t.forward(100)
