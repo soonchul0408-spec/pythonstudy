b69af92f484c8feeedc8d92d582d6c13c0838e0e
백순철 <soonchul0408@gmail.com>
2026-07-18 18:44:10 +0900

    오전 오후 판별코드

--- day0718.py
+++ day0718.py
@@ -1,6 +1,8 @@
-# 로또 과제 만들어보기 
-import random
-
-numbers = random.sample(range(1, 46), 5)
-
-print(numbers)
+# 현재 시각이 오전인지 오후인지 구하는 코드 작성 
+import datetime
+now = datetime.datetime.now()
+if now.hour <12:
+    print("현재 시각은 오전입니다.")
+else:
+    print("현재 시각은 오후입니다.")
+    
