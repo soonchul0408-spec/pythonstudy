02d29882984a010160f7416276ba3e225c548707
백순철 <soonchul0408@gmail.com>
2026-07-19 15:32:17 +0900

    소수 찾기

--- day0719.py
+++ day0719.py
@@ -1,4 +1,16 @@
+import time
 
+n = int(input("자연수 > "))
+start_time = time.time()
 
-for i in range(2, 11, 2): 
-    print(i)
+if n <= 1:
+    print(f"{n}은 소수가 아닙니다.")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print(f"{n}은 {i}로 나누어 떨어집니다.")
+            break
+    else:
+        print(f"{n}은 소수입니다.")
+
+print("걸린 시간 :", time.time() - start_time)
