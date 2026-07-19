7099c60a8578eabddef61ee4f9622895625d5b98
백순철 <soonchul0408@gmail.com>
2026-07-19 14:52:39 +0900

    계절판별식

--- day0719.py
+++ day0719.py
@@ -1,10 +1,10 @@
 import datetime 
 now = datetime.datetime.now()
 if 3 <= now.month <=5: 
-    print("이번달은 {month}월로 봄입니다.".format(MONTH=now.month))
+    print("이번달은 {MONTH}월로 봄입니다.".format(MONTH=now.month))
 elif 6 <= now.month <= 8:
-    print("이번달은 {month}월로 여름입니다.".format(MONTH=now.month))
+    print("이번달은 {MONTH}월로 여름입니다.".format(MONTH=now.month))
 elif 9 <= now.month <= 11:
-    print("이번달은 {month}월로 가을입니다.".format(MONTH=now.month))
+    print("이번달은 {MONTH}월로 가을입니다.".format(MONTH=now.month))
 else:
-    print("이번달은 {month}월로 겨울입니다.".format(MONTH=now.month))
+    print("이번달은 {MONTH}월로 겨울입니다.".format(MONTH=now.month))
