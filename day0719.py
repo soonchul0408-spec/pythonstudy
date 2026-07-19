180d0e89567f6fd6f76fdb8d399898b84d2a2407
백순철 <soonchul0408@gmail.com>
2026-07-19 15:02:16 +0900

    range와 숫자 판별

--- day0719.py
+++ day0719.py
@@ -1,10 +1,4 @@
-import datetime 
-now = datetime.datetime.now()
-if 3 <= now.month <=5: 
-    print("이번달은 {MONTH}월로 봄입니다.".format(MONTH=now.month))
-elif 6 <= now.month <= 8:
-    print("이번달은 {MONTH}월로 여름입니다.".format(MONTH=now.month))
-elif 9 <= now.month <= 11:
-    print("이번달은 {MONTH}월로 가을입니다.".format(MONTH=now.month))
-else:
-    print("이번달은 {MONTH}월로 겨울입니다.".format(MONTH=now.month))
+
+
+for i in range(2, 11, 2): 
+    print(i)
